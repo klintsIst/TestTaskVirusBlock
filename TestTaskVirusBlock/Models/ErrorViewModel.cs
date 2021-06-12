413a788a673c77dc827eeb265d087bb1704eb36b
sergKlints <sergklinov1995@gmail.com>
2021-06-12 19:33:19 +0300

    - Project created

---
+++ TestTaskVirusBlock/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TestTaskVirusBlock.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
